941e7c2f6ec5d6a9b2637a7d64db41b5055fcd78
Dennis Brakhane <dennis.brakhane@inoio.de>
2020-02-13 19:53:55 +0100

    formatting

--- cosinnus_cloud/hooks.py
+++ cosinnus_cloud/hooks.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 import logging
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, Future
 from contextlib import wraps
 from time import sleep
 
@@ -15,12 +15,13 @@ from cosinnus.models.group import CosinnusGroup
 
 from .utils import nextcloud
 
-logger = logging.getLogger('cosinnus')
+logger = logging.getLogger("cosinnus")
+
 
 executor = ThreadPoolExecutor(max_workers=64, thread_name_prefix="nextcloud-req-")
 
-def submit_with_retry(fn, *args, **kwargs):
 
+def submit_with_retry(fn, *args, **kwargs):
     @wraps(fn)
     def exec_with_retry():
         # seconds to wait before retrying.
@@ -32,19 +33,30 @@ def submit_with_retry(fn, *args, **kwargs):
                 try:
                     delay = retry_wait.pop(0)
                     logger.warn(
-                        "Nextcloud call %s(%s, %s) failed. Retrying in %ds (%d tries left)", 
-                        fn.__name__, args, kwargs, delay, len(retry_wait), 
-                        exc_info=True
+                        "Nextcloud call %s(%s, %s) failed. Retrying in %ds (%d tries left)",
+                        fn.__name__,
+                        args,
+                        kwargs,
+                        delay,
+                        len(retry_wait),
+                        exc_info=True,
                     )
-                    sleep(delay)    
+                    sleep(delay)
                     continue
                 except IndexError:
-                    logger.warn("Nextcloud call %s(%s, %s) failed. Giving up", fn.__name__, args, kwargs, exc_info=True)
+                    logger.warn(
+                        "Nextcloud call %s(%s, %s) failed. Giving up",
+                        fn.__name__,
+                        args,
+                        kwargs,
+                        exc_info=True,
+                    )
                     raise e
 
     executor.submit(exec_with_retry).add_done_callback(nc_req_callback)
 
-def nc_req_callback(future):
+
+def nc_req_callback(future: Future):
     try:
         res = future.result()
     except Exception as e:
@@ -56,50 +68,70 @@ def nc_req_callback(future):
 @receiver(signals.user_joined_group)
 def user_joined_group_receiver_sub(sender, user, group, **kwargs):
     """ Triggers when a user properly joined (not only requested to join) a group """
-    logger.debug('User [%s] joined group [%s], adding him/her to Nextcloud', full_name(user), group.name)
-    submit_with_retry(nextcloud.add_user_to_group, f"wechange-{user.id}", f"wechange-{group.name}")
-    
+    logger.debug(
+        "User [%s] joined group [%s], adding him/her to Nextcloud",
+        full_name(user),
+        group.name,
+    )
+    submit_with_retry(
+        nextcloud.add_user_to_group, f"wechange-{user.id}", f"wechange-{group.name}"
+    )
+
 
 @receiver(signals.user_left_group)
 def user_left_group_receiver_sub(sender, user, group, **kwargs):
     """ Triggers when a user left a group """
-    logger.debug('User [%s] left group [%s], removing him from Nextcloud', full_name(user), group.name)
-    submit_with_retry(nextcloud.remove_user_from_group, f"wechange-{user.id}", f"wechange-{group.name}")
-    
+    logger.debug(
+        "User [%s] left group [%s], removing him from Nextcloud",
+        full_name(user),
+        group.name,
+    )
+    submit_with_retry(
+        nextcloud.remove_user_from_group,
+        f"wechange-{user.id}",
+        f"wechange-{group.name}",
+    )
+
+
 # TODO: replace with _created once core PR#23 is merged
 @receiver(signals.userprofile_ceated)
 def userprofile_created_sub(sender, profile, **kwargs):
     user = profile.user
     logger.debug("Userprofile created, adding user [%s] to nextcloud ", full_name(user))
     submit_with_retry(
-        nextcloud.create_user, 
-        f"wechange-{user.id}", 
-        full_name(user), 
-        user.email, 
-        [f"wechange-{group.name}" for group in CosinnusGroup.objects.get_for_user(user)]
+        nextcloud.create_user,
+        f"wechange-{user.id}",
+        full_name(user),
+        user.email,
+        [
+            f"wechange-{group.name}"
+            for group in CosinnusGroup.objects.get_for_user(user)
+        ],
     )
-    
-# TODO: replace with _created once core PR#23 is merged
-@receiver(signals.group_object_ceated)
+
+
+@receiver(signals.group_object_created)
 def group_created_sub(sender, group, **kwargs):
     logger.debug("Creating new group [%s] in Nextcloud", group.name)
 
     submit_with_retry(nextcloud.create_group, f"wechange-{group.name}")
     submit_with_retry(nextcloud.create_group_folder, f"wechange-{group.name}")
 
+
 # maybe listen to user_logged_in and user_logged_out too?
 # https://docs.djangoproject.com/en/3.0/ref/contrib/auth/#django.contrib.auth.signals.user_logged_in
 
+
 @receiver(signals.user_deactivated)
 def user_deactivated(sender, user, **kwargs):
-    logger.warn('User %s was deactivated' % user.username)
+    logger.warn("User %s was deactivated" % user.username)
 
 
 @receiver(signals.user_activated)
 def user_activated(sender, user, **kwargs):
-    logger.warn('User %s was activated' % user.username)
-    
-    
+    logger.warn("User %s was activated" % user.username)
+
+
 @receiver(signals.group_deactivated)
 def group_deactivated(sender, group, **kwargs):
     logger.warn('Group "%s" was deactivated' % group.slug)
@@ -108,5 +140,3 @@ def group_deactivated(sender, group, **kwargs):
 @receiver(signals.group_reactivated)
 def group_reactivated(sender, group, **kwargs):
     logger.warn('Group "%s" was reactivated' % group.slug)
-    
-    
