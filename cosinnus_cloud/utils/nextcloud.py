034f1f252d3ee0e947e246cfe53b60f12a8d2d9f
Dennis Brakhane <brakhane@gmail.com>
2020-04-06 14:36:08 +0200

    fixes

--- cosinnus_cloud/utils/nextcloud.py
+++ cosinnus_cloud/utils/nextcloud.py
@@ -160,10 +160,10 @@ def create_group_folder(name: str, group_id: str, raise_on_existing_name=True) -
     )
 
     if any(folder['mount_point'] == name for folder in response.data.values()):
-        if raise_on_duplicate_name:
+        if raise_on_existing_name:
             raise ValueError("A groupfolder with that name already exists")
         else:
-            logger.debug("group folder [%s] already exists, doing nothing", name)
+            logger.warn("group folder [%s] already exists, doing nothing", name)
             return
     response = _response_or_raise(
         requests.post(
