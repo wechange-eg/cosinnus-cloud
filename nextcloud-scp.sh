360e65f11a547eef655f8b3b21f6f4e128b9061d
sascha <sn@sinnwerkstatt.com>
2020-08-04 16:59:05 +0200

    Nextcloud: Fix scp rsync commands and overriden style file locations

--- nextcloud-scp.sh
+++ nextcloud-scp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rsync -vre ssh ../cosinnus-cloud/nextcloud/* root@<nextcloud-server>.de:/srv/nextcloud/<domain>/app/
-rsync -vre ssh -r ./nextcloud/* root@<nextcloud-server>:/srv/nextcloud/<domain>/app/
+rsync -vre ssh ./nextcloud/* root@<nextcloud-server>:/srv/nextcloud/<domain>/app/
